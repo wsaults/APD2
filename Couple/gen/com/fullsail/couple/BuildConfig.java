f7d229bf24a12cff6ff9eb9f5a3810ae3d5e8b13
William Saults <will@appventur.es>
2013-09-11 20:57:33 -0500

    Added intent to invite activity

---
+++ Couple/gen/com/fullsail/couple/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fullsail.couple;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- Couple/src/com/fullsail/couple/MainActivity.java
+++ Couple/src/com/fullsail/couple/MainActivity.java
@@ -2,7 +2,10 @@ package com.fullsail.couple;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button registerButton = (Button) findViewById(R.id.registerButton);
+		registerButton.setOnClickListener(new View.OnClickListener() { 
+			@Override
+			public void onClick(View v) {
+				Intent a = new Intent(getApplicationContext(),InviteActivity.class);
+				a.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+				setResult(RESULT_OK, a);
+				startActivityForResult(a,0);
+		    }
+		});
 	}
 
 	@Override
