30fb23d481d3947e0b46d9b5ee618295468a8fba
William Saults <will@appventur.es>
2013-09-16 21:45:29 -0500

    Added Parse. Implemented register and login functionality. Added login button to register/login view.

--- Couple/gen/com/fullsail/couple/R.java
+++ Couple/gen/com/fullsail/couple/R.java
@@ -24,29 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ImageButton01=0x7f080016;
-        public static final int ImageButton02=0x7f080017;
-        public static final int TextView01=0x7f080009;
-        public static final int TextView02=0x7f08000b;
-        public static final int TextView03=0x7f08000d;
-        public static final int action_settings=0x7f080018;
+        public static final int action_settings=0x7f08000c;
         public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080004;
-        public static final int imageButton1=0x7f08000f;
-        public static final int imageButton3=0x7f080010;
-        public static final int imageButton4=0x7f080011;
-        public static final int imageButton5=0x7f080012;
-        public static final int imageButton6=0x7f080013;
-        public static final int imageButton7=0x7f080014;
-        public static final int imageButton8=0x7f080015;
-        public static final int pager=0x7f080006;
-        public static final int registerButton=0x7f080005;
-        public static final int section_label=0x7f08000e;
+        public static final int emailEditText=0x7f080004;
+        public static final int gridView1=0x7f08000b;
+        public static final int listView1=0x7f080009;
+        public static final int loginButton=0x7f080006;
+        public static final int pager=0x7f080008;
+        public static final int passwordEditText=0x7f080005;
+        public static final int registerButton=0x7f080007;
+        public static final int section_label=0x7f08000a;
         public static final int sendInviteButton=0x7f080002;
-        public static final int tableRow1=0x7f080007;
-        public static final int tableRow2=0x7f080008;
-        public static final int tableRow3=0x7f08000a;
-        public static final int tableRow4=0x7f08000c;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080003;
     }
@@ -70,19 +58,20 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int email=0x7f050004;
+        public static final int email=0x7f050005;
         public static final int hello_world=0x7f050002;
-        public static final int invite_email_button_text=0x7f050008;
-        public static final int invite_email_text=0x7f050007;
-        public static final int invite_text=0x7f050006;
-        public static final int password=0x7f050005;
+        public static final int invite_email_button_text=0x7f050009;
+        public static final int invite_email_text=0x7f050008;
+        public static final int invite_text=0x7f050007;
+        public static final int login=0x7f050004;
+        public static final int password=0x7f050006;
         public static final int register=0x7f050003;
-        public static final int title_activity_invite=0x7f05000a;
-        public static final int title_activity_main=0x7f050009;
-        public static final int title_activity_photo=0x7f05000e;
-        public static final int title_activity_tab=0x7f05000d;
-        public static final int title_section1=0x7f05000b;
-        public static final int title_section2=0x7f05000c;
+        public static final int title_activity_invite=0x7f05000b;
+        public static final int title_activity_main=0x7f05000a;
+        public static final int title_activity_photo=0x7f05000f;
+        public static final int title_activity_tab=0x7f05000e;
+        public static final int title_section1=0x7f05000c;
+        public static final int title_section2=0x7f05000d;
     }
     public static final class style {
         /** 
--- Couple/src/com/fullsail/couple/MainActivity.java
+++ Couple/src/com/fullsail/couple/MainActivity.java
@@ -9,27 +9,65 @@
  */
 package com.fullsail.couple;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class MainActivity extends Activity {
+	
+	EditText _emailEditText;
+	EditText _passwordEditText;
+	Context _context;
+	SharedPreferences _preferences;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Parse.initialize(this, "7LdGck89MP9UnBeem9wYc1ER96MMOJKfGfWlP6rC", "KGEopcwTYaXZ2FUAmjw5aZwIPRTjw522TjJPya4H"); 
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+		_context = this;
+		_preferences = _context.getSharedPreferences("MyPreferences", MODE_PRIVATE);
+		_emailEditText = (EditText) findViewById(R.id.emailEditText);
+		_passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+		
+		// Restore the text in the edit text.
+		String userName = _preferences.getString("username", "");
+		if (userName.length() > 0) {
+			_emailEditText.setText(userName);
+		}
 
 		Button registerButton = (Button) findViewById(R.id.registerButton);
 		registerButton.setOnClickListener(new View.OnClickListener() { 
 			@Override
 			public void onClick(View v) {
-				Intent a = new Intent(getApplicationContext(),InviteActivity.class);
-				setResult(RESULT_OK, a);
-				startActivityForResult(a,0);
+				createNewUser(_emailEditText.getText().toString(), _passwordEditText.getText().toString());
+		    }
+		});
+		
+		Button loginButton = (Button) findViewById(R.id.loginButton);
+		loginButton.setOnClickListener(new View.OnClickListener() { 
+			@Override
+			public void onClick(View v) {
+				loginUser(_emailEditText.getText().toString(), _passwordEditText.getText().toString());
 		    }
 		});
 	}
@@ -40,5 +78,75 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public Boolean areUserCredentialsValid(String username, String password) {
+		// Make sure there is valid data in the fields
+		if (username.length() <= 0) {
+			// TODO: display error message.
+			return false;
+		}
 
+		if (password.length() <= 0) {
+			// TODO: display error message.
+			return false;
+		}
+
+		// TODO: do email validation.
+		return true;
+	}
+	
+	public void createNewUser(final String username, final String password) {
+		if (!areUserCredentialsValid(username, password)) return;
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+		query.whereEqualTo("username", _emailEditText.getText().toString());
+		query.whereEqualTo("password", _passwordEditText.getText().toString());
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> user, ParseException e) {
+				if (e == null) {
+		            Log.d("user", "Retrieved " + user.size() + " user");
+		        } else {
+		            Log.d("user", "Creating user: " + e.getMessage());
+		            // Save the username locally
+		    		SharedPreferences.Editor editor = _preferences.edit();
+		    		editor.putString("username", username);
+		    		editor.commit();
+		    		
+		    		// Save the username and password to parse
+		    		ParseObject userObject = new ParseObject("CoupleUser");
+		    		userObject.put("username", username);
+		    		userObject.put("password", password);
+		    		userObject.saveInBackground();
+		    		
+		    		Intent a = new Intent(getApplicationContext(),InviteActivity.class);
+					setResult(RESULT_OK, a);
+					startActivityForResult(a,0);
+		        }	
+			}
+		});
+	}
+	
+	public void loginUser(String username, String password) {
+		if (!areUserCredentialsValid(username, password)) return;
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+		query.whereEqualTo("username", _emailEditText.getText().toString());
+		query.whereEqualTo("password", _passwordEditText.getText().toString());
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> user, ParseException e) {
+				if (e == null) {
+		            Log.d("user", "Retrieved " + user.size() + " user");
+		            
+		            // A valid user, matching username and password, was found. Login.
+		            Intent a = new Intent(getApplicationContext(),InviteActivity.class);
+					setResult(RESULT_OK, a);
+					startActivityForResult(a,0);
+		        } else {
+		            Log.d("user", "No matching credentials found: " + e.getMessage());
+		        }	
+			}
+		});
+	}
 }
